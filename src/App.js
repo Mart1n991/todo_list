02cdd6ff98e37aa6743afdaa439a62a693019ec5
Mart1n991 <martinjavorsky8@gmail.com>
2020-11-09 20:32:09 +0100

    Do App.js som pridal state todos a inputText

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
-
-//Import icons from font awesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 
 //Import components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="app-container">
       <h1 className="app-heading">TO-DO LIST</h1>
-      <Form />
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+      />
       <TodoList />
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Todo from "./Todo";
 
 export default function TodoList() {
   return (
     <div className="todo-list">
-      <ul>
-        <Todo />
-      </ul>
+      <ul></ul>
     </div>
   );
 }

6812f625b0adea237cfac9f6b0584b05d405f0eb
Mart1n991 <martinjavorsky8@gmail.com>
2020-11-09 20:31:31 +0100

    Pridal som funkcie pre update statu inputText a statu todos

---
+++ src/components/Form.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+//Import icons from font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+
+export default function Form({ inputText, setInputText, todos, setTodos }) {
+  // Set inputText value
+  const onInputTextChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setTodos([
+      ...todos,
+      {
+        todos: inputText,
+        completed: false,
+        id: Math.random() * 1000,
+      },
+    ]);
+
+    setInputText("");
+  };
+
+  console.log("todos ", todos);
+
+  return (
+    <form>
+      <input
+        placeholder="pridaj úlohu"
+        type="text"
+        value={inputText}
+        onChange={onInputTextChange}
+      ></input>
+      <button className="btn-add" onClick={onSubmit}>
+        <FontAwesomeIcon icon={faPlusCircle} />
+      </button>
+
+      <select className="filtered-todos">
+        <option className="all-todos">Všetky</option>
+        <option className="completed-todos">Dokončené</option>
+        <option className="uncompleted-todos">Nedokončené</option>
+      </select>
+    </form>
+  );
+}

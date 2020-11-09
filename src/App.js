d7940fa33aa1b5bf91d1cd41e517e2ac59601872
Mart1n991 <martinjavorsky8@gmail.com>
2020-11-09 19:49:05 +0100

    vytvorenie formulára pre zadávanie a filtrovanie todos v app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+//Import icons from font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
-  return <div className="App">My first App</div>;
+  return (
+    <div className="app-container">
+      <h1 className="app-heading">TO-DO LIST</h1>
+      <form>
+        <input placeholder="pridaj úlohu"></input>
+        <button className="btn-add">
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+
+        <select>
+          <option>Všetky</option>
+          <option>Dokončené</option>
+          <option>Nedokončené</option>
+        </select>
+      </form>
+    </div>
+  );
 }
 
 export default App;

a815f91aaaf184c9e6c7593d2e1f04a4be403233
Mart1n991 <martinjavorsky8@gmail.com>
2020-11-13 19:34:07 +0100

    doplnil som state isActive pomocou ktoreho priradujem classu elementu todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Import icons from font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckSquare, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Todo({ todoName, todo, todos, setTodos }) {
+  const [isActive, setIsActive] = useState(false);
   //Function to delete todo
   const deleteTodo = () => {
-    setTodos(todos.filter((element) => element.id !== todo.id));
+    setIsActive(true);
+
+    setTimeout(function () {
+      setTodos(todos.filter((element) => element.id !== todo.id));
+    }, 1000);
   };
 
   //Function to add completestatus to true
@@ -26,7 +31,7 @@ export default function Todo({ todoName, todo, todos, setTodos }) {
   };
 
   return (
-    <div className="todo">
+    <div className={`todo ${isActive ? "todo-fall" : " "}`}>
       <p
         className={`todo-name ${
           todo.completed ? "todo-completed" : "todo-uncompleted"
